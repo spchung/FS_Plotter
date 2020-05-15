f54137b60c64d204e6d390e728bffca3c9103843
Stephen Chung <stephen@Stevos-Macbook.local>
2020-05-15 14:54:21 -0700

    Updates

---
+++ precache-manifest.4abfc47683678eae1bee221bb4ae9a6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b778729b68c03ac64f75910223777af5",
+    "url": "/spchung/FS_Plotter/index.html"
+  },
+  {
+    "revision": "cf95250bbef9889c9dcc",
+    "url": "/spchung/FS_Plotter/static/css/2.6883e14e.chunk.css"
+  },
+  {
+    "revision": "2f17506a1d2d53384000",
+    "url": "/spchung/FS_Plotter/static/css/main.a15c1ff6.chunk.css"
+  },
+  {
+    "revision": "cf95250bbef9889c9dcc",
+    "url": "/spchung/FS_Plotter/static/js/2.bdad41cb.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/spchung/FS_Plotter/static/js/2.bdad41cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f17506a1d2d53384000",
+    "url": "/spchung/FS_Plotter/static/js/main.bdc5f538.chunk.js"
+  },
+  {
+    "revision": "e4a95a74b6df0b7e334f",
+    "url": "/spchung/FS_Plotter/static/js/runtime-main.c199c47f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spchung/plotter/FS_Plotter/precache-manifest.3f9c50a03849805acaa1b085623a064c.js"
+  "/spchung/FS_Plotter/precache-manifest.4abfc47683678eae1bee221bb4ae9a6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spchung/plotter/FS_Plotter/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spchung/FS_Plotter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
