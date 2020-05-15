bc2c4d90e9b3e9d35b01251bf6a2e52ce3d2e8ae
Stephen Chung <stephen@Stevos-Macbook.local>
2020-05-15 14:40:37 -0700

    deploy

---
+++ build/precache-manifest.b3d057c9013e3a2b02651ed354597eaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79616eebff443bd426109c4dccf899c",
+    "url": "/spchung/plotter/Fullstack-plotter/index.html"
+  },
+  {
+    "revision": "7b0e3da2cc8664207a1d",
+    "url": "/spchung/plotter/Fullstack-plotter/static/css/2.6883e14e.chunk.css"
+  },
+  {
+    "revision": "2f17506a1d2d53384000",
+    "url": "/spchung/plotter/Fullstack-plotter/static/css/main.a15c1ff6.chunk.css"
+  },
+  {
+    "revision": "7b0e3da2cc8664207a1d",
+    "url": "/spchung/plotter/Fullstack-plotter/static/js/2.50a0115e.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/spchung/plotter/Fullstack-plotter/static/js/2.50a0115e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f17506a1d2d53384000",
+    "url": "/spchung/plotter/Fullstack-plotter/static/js/main.bdc5f538.chunk.js"
+  },
+  {
+    "revision": "3a5fc6b83a8266d74fe5",
+    "url": "/spchung/plotter/Fullstack-plotter/static/js/runtime-main.b5b0c264.js"
+  }
+]);
